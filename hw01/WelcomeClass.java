5b13b7247bd8251af489f0898330f34edb3d8c81
njh217 <njh217@lehigh.edu>
2015-08-29 20:49:10 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////////////////////////
+//Nick Hirdt
+//HW 01
+//Welcome Class Java Program 
+//
+//  first complile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//define a class
+public class WelcomeClass{ 
+
+//  add main method
+  public static void main(String[] args) {
+       
+//  print personalized welcome message
+    System.out.println("      -----------");
+    System.out.println("      | WELCOME |");
+    System.out.println("   ^  ^  ^  ^  ^  ^");
+    System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println(" <-n--j--h--2--1--7->");
+    System.out.println("  \\ / \\ / \\ / \\ / \\ / ");
+    System.out.println("   v   v   v   v   v  ");
+    System.out.println("This summer I lived at lehigh.");
+    System.out.println("In a house on Montclair ave.");
+    
+    }
+  }
