ab862cbad1f132b21a67497a7343c2c8d4efe863
njh217 <njh217@lehigh.edu>
2015-10-15 23:35:46 +0000

    updated lab07

--- lab07/encrypted_x.java
+++ lab07/encrypted_x.java
@@ -14,48 +14,60 @@ public class encrypted_x{
 //an integer to store the user input
 double myInt = 0;
 //swtiches to decide if input is correct
-boolean acceptable = false;
-// loop unitl acceptable input (interger between 0 and 100)
-System.out.print("Input an interger between 0-100:  ");
+//check to verify good length and width values
+boolean lengthInt = false;
+boolean lengthRange = false;
+//loop until you get acceptable length (i.e. if it's an intereger less than 80)
+System.out.print("Input your desired size: ");
 Scanner myScanner = new Scanner( System.in );
-while( !acceptable ){
-    //check if input is an interger
-    if ( myScanner.hasNextInt() ){
-        //if so, check its in the correct range
-        myInt = myScanner.nextInt();
-        if ( myInt >= 0 && myInt <= 100 ){
-            acceptable = true;
+//infinite loop only broken when length value is valid
+while ( lengthInt == false | lengthRange == false){ 
+    if (!myScanner.hasNextInt()){  //If non valid interger is input enter loop
+    System.out.println("    Error. Please type an integer."); 
+    System.out.print("Please type desired size: ");
+    myScanner.next();  //Scanner looks for next input
+    }
+    else if (myScanner.hasNextInt()){  //if valid double is input
+    myInt = myScanner.nextInt();  //store monday expenses as next double
+        if( myInt < 0 | myInt > 100 ){ //if expenses monday is less than 0 return 0
+	    System.out.println("Please only enter a value between 0 and 80.");
+	    System.out.print("Please type desired length: ");
         }
-        else{
-            System.out.println("    ERROR: need a value between 0-100");
-            System.out.print("Input your interger between 0-100:  ");
-            myScanner.next();
-            return;
+        else{  //if good value change boolean values to exit loop
+            lengthInt = true;
+            lengthRange = true;
+        break; //break from while loop if valid int and between 0-80
         }
     }
-    else{
-        System.out.println("    ERROR: need an integer");
-        System.out.print("Input your interger between 0-100:  ");
-        myScanner.next();
-    }
+}
+// loop unitl acceptable input (interger between 0 and 100)
+
 //print outer loop
-for(int i = 0; i < myInt; i++){
-    //print inner loop
-    for(int j = 0; j < myInt; j++){
-        if ( i == j ){
+for(int row = 1; row <= myInt; row++ ){ //loops for height
+    for(int col = 1; col <= myInt; col++ ){ //loops for length
+        if( row == col ){ //print # for first case
             System.out.print(" ");
-            j++;
         }
-        else if ( j == (myInt - i) ){
-            System.out.print("-");
-            j++;
+        
+        else if(( col - row ) == myInt){  // prints \
+            System.out.print(" ");
+        }
+        else if(((col + row) - 1) % myInt == 0){
+            if((row + col ) % (myInt + 1) == 0){
+                System.out.print(" ");
+            }
+            else{
+            System.out.print("X"); //prints # for second case
+            }
         }
         else{
-            System.out.print("*");
+            System.out.print("X");
         }
+     if ( col == myInt ){
+        System.out.println(); //begins next row after finishing printing previous row
+    }   
     }
-    System.out.println(" ");
 }
-}
-}  //end of main method   
+
+} //end of main method   
       } //end of class
