b2956e0ae4e58f1f94f0d59d1dc364af0961141d
njh217 <njh217@lehigh.edu>
2015-11-21 20:13:59 +0000

    completed lab04

--- expermenting/experiment01.java
+++ expermenting/experiment01.java
@@ -1,17 +1,40 @@
 
-import java.util.Scanner;  //import scanner
+//import java.util.Scanner;  //import scanner
 public class experiment01{
-    public static void main(String[] args) {
     
-int i = 0, j = 0, k = 0;
-for(k = 2; k < 5; k++){
-    for(j = 0; j < k ; j++){
-        for(i = 0; i < j + 1; i++){
-            System.out.print("b");
-        }
-        System.out.println("");
-    }
+    public static int[] centroidCalculator( int[] x){
+    
+       int length = x.length;
+       
+       int xtot = 0;
+       int ytot = 0;
+       int ztot = 0;
+       
+       for(int i = 0; i < length; i++){
+           if(i == 2 || i % 5 == 0){
+               ztot += x[i];
+           }
+           else if(i == 1 || i % 4 == 0){
+               ytot += x[i];
+           }
+           else if(i == 0 || i % 3 == 0){
+               xtot += x[i];
+           }
+       }
+       int xval = xtot / (length / 3);
+       int yval = ytot / (length / 3);
+       int zval = ztot / (length / 3);
+       int[] centroid = {xval, yval, zval};
+       for(int j = 0; j < 3; j++){
+           System.out.print(centroid[j] + " ");
+       }
+       return centroid;
 }
+    
+    public static void main(String[] args) {
 
+
+int X[] = {1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3};
+centroidCalculator(X);
 }
 }
--- lab06/GetLoopy.java
+++ lab06/GetLoopy.java
@@ -10,8 +10,8 @@ public class GetLoopy{
 
 //Step 1
 System.out.println("Step 1:");
-int i = 1;
-int j = 1;
+int i = 0;
+int j = 0;
 while ( i <= 7 ){
         System.out.print( i );
         i++;
@@ -21,23 +21,64 @@ while ( j <= 7 ){
         j++;
     }
 System.out.println();
+int checker = 0;
 //step 2
-int inputValue = 10;
 System.out.println("Step 2:");
-while ( inputValue <= 100 ){
-        int myFactor = 2;
-    while( myFactor < inputValue ){
-	        if ( (inputValue % myFactor == 0) ){
-		    break;
-	        }
-	        else if (!(inputValue % myFactor == 0 && myFactor == 99) ){
-	        System.out.print( inputValue + " ");
-	        }
-	    myFactor++;
-	    break;
+//While loop portion
+System.out.print("WHILE LOOP: ");
+int counterWL = 10;
+while( counterWL < 101 ){
+    checker = 2;
+    while(checker < counterWL){
+        if(counterWL % checker == 0){
+            break;
+        }
+        if(checker == counterWL - 1){
+            System.out.print(counterWL + " ");
+        }
+        checker ++;
     }
-inputValue++;
+    counterWL ++;
 }
-        
+System.out.println();
+//For loop portion
+System.out.print("FOR LOOP: ");
+for(counterWL = 2; counterWL < 101; counterWL++){
+    for(checker = 2; checker < counterWL; checker++){
+        if(counterWL % checker == 0){
+            break;
+        }
+        if(checker == counterWL - 1){
+            System.out.print(counterWL + " ");
+        }
+    }
+}
+System.out.println();
+//do while loop portion
+System.out.print("DO WHILE LOOP: ");
+counterWL = 2;
+do{
+    checker = 2;
+    do{
+        if(counterWL % checker == 0){
+            break;
+        }
+        if(checker == counterWL - 1){
+            System.out.print(counterWL + " ");
+        }
+        checker ++;
+    }while(checker < counterWL);
+    counterWL ++;
+}while(counterWL < 101);
+System.out.println();
+System.out.println("Step 3:");
+int limit = (int)(Math.random()*195)+5;
+for (int q = 1; q < limit + 1; q++){
+    System.out.print(":o ");
+    if(q % 20 == 0){
+        System.out.println();
+    }
+}
+System.out.println();
     }
 }
